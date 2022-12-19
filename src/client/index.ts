3aa6c3ab69fc317da4c86ff7edf4f7d041c9c3ea
William Wolff <william.wolff@iohk.io>
2022-12-19 19:55:20 +0100

    feat: add init call for client with filter

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,6 +3,7 @@ require('dotenv').config();
 import { credentials } from '@grpc/grpc-js';
 import swap from '../grpc';
 import { SwapResponse } from '../proto/swap/SwapResponse';
+import { TradingPair__Output } from '../proto/swap/TradingPair';
 import { grpc_client_log } from '../utils/print';
 
 const client = new swap.Swap(
@@ -19,12 +20,40 @@ client.waitForReady(deadline, (err) => {
   }
   grpc_client_log('Connected to server');
 
+  const errorHandler = (err: Error) => grpc_client_log(err.message);
   const liquidityStream = client.Liquidity({}); // no token filters passed
   liquidityStream
-    .on('error', (err) => grpc_client_log(err.message))
-    .on('data', (response: SwapResponse) => {
-      grpc_client_log(`New Data:`);
-      console.log(response);
+    .on('error', errorHandler)
+    .on('data', (pair: TradingPair__Output) => {
+      grpc_client_log(`Liquidity Change:`);
+      grpc_client_log(
+        `(${pair.a?.name}/${pair.b?.name}): Price ${pair.a?.amount?.div(
+          pair.b!.amount!
+        )}`
+      );
     })
     .on('end', () => grpc_client_log(`Disconnected`));
+
+  const tokens: string[] = [];
+  client
+    .Init({ tokens }, (err, res) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      if (!res?.pairs) {
+        grpc_client_log(`No liquidity for [${tokens}]`);
+        return;
+      }
+      grpc_client_log('Init Liquidity');
+      res?.pairs?.forEach((pair) => {
+        grpc_client_log(
+          `(${pair.a?.name}/${pair.b?.name}): Price ${pair.a?.amount?.div(
+            pair.b!.amount!
+          )}`
+        );
+      });
+    })
+    .on('error', errorHandler);
 });
